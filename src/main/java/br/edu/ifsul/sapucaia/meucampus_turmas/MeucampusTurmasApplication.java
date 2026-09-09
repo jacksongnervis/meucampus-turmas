60379e027dbb98dd861b9e2f58b1b21b917a993e
Jackson Gonçalves <jacksonnervis@gmail.com>
2026-09-08 21:26:33 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/br/edu/ifsul/sapucaia/meucampus_turmas/MeucampusTurmasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.sapucaia.meucampus_turmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucampusTurmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucampusTurmasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsul/sapucaia/meucampus_turmas/MeucampusTurmasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.sapucaia.meucampus_turmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeucampusTurmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
