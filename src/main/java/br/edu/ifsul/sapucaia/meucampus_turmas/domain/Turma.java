ea67ddf77fb2e2475d8d10ae868562f55a575392
NathanDoile <doile.souza@gmail.com>
2026-09-11 21:48:28 -0300

    Adicionando informmacoes da coluna

--- src/main/java/br/edu/ifsul/sapucaia/meucampus_turmas/domain/Turma.java
+++ src/main/java/br/edu/ifsul/sapucaia/meucampus_turmas/domain/Turma.java
@@ -1,5 +1,6 @@
 package br.edu.ifsul.sapucaia.meucampus_turmas.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,21 +18,30 @@ public class Turma {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(nullable = false, unique = true)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String codigo;
 
+    @Column(nullable = false)
     private String nomeDisciplina;
 
+    @Column(nullable = false)
     private String idProfessor;
 
+    @Column(nullable = false)
     private int semestre;
 
+    @Column(nullable = false)
     private String sala;
 
+    @Column(nullable = false)
     private LocalTime horario;
 
+    @Column(nullable = false)
     private int cargaHoraria;
 
+    @Column(nullable = false)
     private int numeroVagas;
 }
