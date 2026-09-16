fe9a2dabe96bb3335ec0d547530cbb912fd1f9aa
jacksonnnervis <jacksonnervis@gmail.com>
2026-09-15 21:24:41 -0300

    refactor: arruma exceptions

--- src/main/java/br/edu/ifsul/sapucaia/meucampus_turmas/service/CadastrarTurmaService.java
+++ src/main/java/br/edu/ifsul/sapucaia/meucampus_turmas/service/CadastrarTurmaService.java
@@ -3,11 +3,12 @@ package br.edu.ifsul.sapucaia.meucampus_turmas.service;
 import br.edu.ifsul.sapucaia.meucampus_turmas.domain.Turma;
 import br.edu.ifsul.sapucaia.meucampus_turmas.dto.TurmaRequestDTO;
 import br.edu.ifsul.sapucaia.meucampus_turmas.dto.TurmaResponseDTO;
-import br.edu.ifsul.sapucaia.meucampus_turmas.exception.BusinessException;
 import br.edu.ifsul.sapucaia.meucampus_turmas.mapper.TurmaMapper;
 import br.edu.ifsul.sapucaia.meucampus_turmas.repository.TurmaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +18,12 @@ public class CadastrarTurmaService {
 
     public TurmaResponseDTO cadastrar(TurmaRequestDTO dto) {
         if (turmaRepository.existsByCodigo(dto.getCodigo().trim())) {
-            throw new BusinessException("Já existe uma turma cadastrada com o código: " + dto.getCodigo());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe uma turma cadastrada com o código: " + dto.getCodigo());
         }
 
         if (dto.getHorarioFinal().isBefore(dto.getHorarioInicial()) ||
                 dto.getHorarioFinal().equals(dto.getHorarioInicial())) {
-            throw new BusinessException("O horário final deve ser posterior ao horário inicial");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O horário final deve ser posterior ao horário inicial");
         }
 
         Turma turma = TurmaMapper.toEntity(dto);
